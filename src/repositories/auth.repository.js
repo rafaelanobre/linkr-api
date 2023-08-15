334059f5066170515e8cdb9e429d032cf57591b3
Willyan <wkinhaamaral@gmail.com>
2023-08-15 19:51:25 -0300

    feat:signin-signup

--- src/repositories/auth.repository.js
+++ src/repositories/auth.repository.js
@@ -1,1 +1,12 @@
 import { db } from "../database/database.connection.js";
+
+
+export async function checkEmail(email){
+    return db.query(`SELECT * FROM users WHERE email=$1`, [email]);
+}
+
+
+export async function newUser(name, email, passwordHashed, photo){
+    return db.query(`INSERT INTO users ( name, email, password, photo) VALUES
+    ($1, $2, $3, $4) RETURNING * `, [name, email, passwordHashed, photo]);
+}
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express"
+import authrouter from "./auth.routes.js";
 
 const router = Router();
+
+router.use(authrouter)
 
 export default router;
