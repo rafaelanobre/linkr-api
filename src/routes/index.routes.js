ac367900d6c6df5819067f7881bce3bbc91359b7
rafaelanobre <raafanobre@gmail.com>
2023-08-15 20:38:54 -0300

    :wrench: Fix: index routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express"
 import authrouter from "./auth.routes.js";
+import postsRouter from "./posts.routes.js";
 
 const router = Router();
 
-router.use(authrouter)
+router.use(authrouter);
+router.use(postsRouter);
 
 export default router;
