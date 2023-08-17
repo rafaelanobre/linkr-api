6238d51f93ae704fc6a36a5f65b99c7591d117b3
HenriqueSilva <HenriqueSilva254@gmail.com>
2023-08-16 21:11:37 -0300

    fix: function getPosts

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -1,44 +1,53 @@
-
 import { db } from "../database/database.connection.js";
 import { createPostDB } from "../repositories/post.repository.js";
 
 export async function publishPostForTimeline(req, res) {
-    const { url, description } = req.body;
-    const createdby = res.locals.userId;
-    const createdat = new Date();
+  const { url, description } = req.body;
+  const createdby = res.locals.userId;
+  const createdat = new Date();
 
-    try {
-        const post = await createPostDB(createdby, createdat, url, description); 
-        
-        res.status(200).send(post.rows[0]);
-    } catch (err) {
-        console.log(err)
-        res.status(500).send(err.message);
-    }
+  try {
+    const post = await createPostDB(createdby, createdat, url, description);
+
+    res.status(200).send(post.rows[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
 }
 
-export async function getPostsForTimeline(req,res){
-    try{
-        const { rows: posts } = await db.query(`
-        SELECT 
-            p.id AS "postId",
-            p.url,
-            p.description,
-            u.name AS "userName",
-            u.photo AS "userPhoto"
-        FROM 
-            posts p
-        LEFT JOIN 
-            users u ON p."createdBy" = u.id
-        ORDER BY 
-            "createdAt" DESC
-        LIMIT 20;
+export async function getPostsForTimeline(req, res) {
+  try {
+    const { rows: posts } = await db.query(`
+    SELECT 
+    p.id AS "postId",
+    p.url,
+    p.description,
+    u.name AS "userName",
+    u.photo AS "userPhoto",
+    ARRAY_AGG(users.name) AS "usersLikedNames"
+FROM 
+    posts p
+LEFT JOIN 
+    users u ON p."createdBy" = u.id
+LEFT JOIN 
+    likes l ON l."postId" = p.id
+LEFT JOIN 
+    users ON l.userliked = users.id
+GROUP BY 
+    p.id, u.id
+ORDER BY 
+    p."createdAt" DESC
+LIMIT 20;
         `);
 
-        if (posts.rowCount === 0) return res.status(204).send({message:'There are no posts yet'});
-        res.status(200).send(posts);
-    }catch(error){
-        const errorMessage = error.message ? error.message : "Ocorreu um erro interno no servidor.";
-        res.status(500).send(errorMessage);
-    }
+    if (posts.rowCount === 0)
+      return res.status(204).send({ message: "There are no posts yet" });
+    res.status(200).send(posts);
+  } catch (error) {
+    const errorMessage = error.message
+      ? error.message
+      : "Ocorreu um erro interno no servidor.";
+    res.status(500).send(errorMessage);
+  }
 }
