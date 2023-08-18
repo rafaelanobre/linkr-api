d1a9a2778b01d8748c12ec549c39bdf134214884
rafaelanobre <raafanobre@gmail.com>
2023-08-18 12:44:14 -0300

    :sparkles: Feat: get posts by hashtag id

--- src/controllers/hashtag.controller.js
+++ src/controllers/hashtag.controller.js
@@ -1,4 +1,5 @@
 import { db } from "../database/database.connection.js";
+import { getMetadata } from "../services/posts.services.js";
 
 export async function trendingHashtags(req,res){
     try{
@@ -7,6 +8,61 @@ export async function trendingHashtags(req,res){
         `)
         res.status(200).send(trending);
     } catch(error){
+        const errorMessage = error.message ? error.message : 'An internal server error occurred.';
+        res.status(500).send(errorMessage);
+    }
+}
+
+export async function postsByHashtag(req,res){
+    const {id} = req.params;
+    try{
+        const { rows: posts } = await db.query(`
+        SELECT 
+            p.id AS "postId",
+            p.url,
+            p.description,
+            u.name AS "userName",
+            u.photo AS "userPhoto",
+            ARRAY_AGG(users.name) AS "usersLikedNames",
+            COALESCE(
+                json_agg(
+                    json_build_object(
+                        'hashtagId', h.id,
+                        'hashtag', h.hashtag
+                    )
+                ) FILTER (WHERE h.id IS NOT NULL),
+                '[]'
+            ) AS hashtags
+        FROM 
+            posts p
+        LEFT JOIN 
+            users u ON p."createdBy" = u.id
+        LEFT JOIN 
+            likes l ON l."postId" = p.id
+        LEFT JOIN 
+            users ON l.userliked = users.id
+        LEFT JOIN
+            "postsHashtags" ph ON p.id = ph."postId"
+        LEFT JOIN
+            hashtags h ON ph."hashtagId" = h.id
+        WHERE
+            h.id = $1
+        GROUP BY
+            p.id, u.id
+        ORDER BY 
+            p."createdAt" DESC
+        LIMIT 20;
+        `, [id])
+
+        if (posts.rowCount === 0) return res.status(204).send({ message: 'There are no posts yet' });
+
+        const postsWithMetadata = await Promise.all(posts.map(async (post) => {
+            const metadata = post.url ? await getMetadata(post.url) : {};
+            return {...post,metadata};
+        }));
+
+        res.status(200).send(postsWithMetadata);
+    }catch(error){
         const errorMessage = error.message ? error.message : 'An internal server error occurred.';
         res.status(500).send(errorMessage);
     }
--- src/routes/hashtags.routes.js
+++ src/routes/hashtags.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { trendingHashtags } from "../controllers/hashtag.controller.js";
+import { postsByHashtag, trendingHashtags } from "../controllers/hashtag.controller.js";
 
 const hashtagsRouter = Router()
 
 hashtagsRouter.get("/trending", trendingHashtags);
+hashtagsRouter.get("/hashtag/:id", postsByHashtag);
 
 export default hashtagsRouter
