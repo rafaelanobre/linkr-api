78668d744cdfaa8ed882f2c9234ff477fa3329b3
rafaelanobre <raafanobre@gmail.com>
2023-08-21 12:08:16 -0300

    :sparkles: Feat: edit posts

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -18,7 +18,8 @@ export async function publishPostForTimeline(req, res) {
         const post = await createPostDB(createdBy, createdAt, url, descriptionWithoutHashtags);
 
         if (hashtags.length > 0) {
-            await Promise.all(hashtags.map(async (tag) => {
+            const uniqueHashtags = Array.from(new Set(hashtags));
+            await Promise.all(uniqueHashtags.map(async (tag) => {
                 const { rows: [existingTag] } = await db.query(`
                     SELECT * FROM hashtags WHERE hashtag = $1;
                 `, [tag]);
@@ -145,4 +146,104 @@ export async function DeletePost(req, res) {
         res.status(500).send(errorMessage);
     }
 
+}
+
+export async function editPost(req,res){
+    const {id} = req.params;
+    const { description } = req.body;
+
+    const hashtagsArray = description.match(/#(\w+)/g) || [];
+    const hashtags = hashtagsArray.map(hashtag => hashtag.replace('#', ''));
+    const descriptionWithoutHashtags = description.replace(/#(\w+)/g, '').trim();
+
+    try{
+        const { rows: [post] } = await db.query(`SELECT * FROM posts WHERE id = $1`, [id]);
+        if(!post) return res.status(404).send({ message: 'Não foi possível encontrar esse post.' });
+
+        if(post.createdBy !== res.locals.userId) return res.status(403).send({ message: 'Você não pode editar esse registro.' });
+        
+        const { rows: [newPost] } = await db.query(`
+        UPDATE posts AS p
+        SET description = $1
+        WHERE id = $2
+        RETURNING p.*,
+            COALESCE(
+                (SELECT json_agg(
+                    json_build_object(
+                        'hashtagId', h.id,
+                        'hashtag', h.hashtag
+                    )
+                ) FROM "postsHashtags" AS ph
+                LEFT JOIN hashtags AS h ON ph."hashtagId" = h.id
+                WHERE ph."postId" = p.id), '[]') AS hashtags;`,
+        [descriptionWithoutHashtags, id]);
+
+
+        await Promise.all(newPost.hashtags.map(async (hashtag) => {
+            if (!hashtags.includes(hashtag.hashtag)) {
+                await db.query(`
+                    DELETE FROM "postsHashtags"
+                    WHERE "postId" = $1 AND "hashtagId" = $2;
+                `, [newPost.id, hashtag.hashtagId]);
+
+                await db.query(`
+                    UPDATE hashtags
+                    SET total = total - 1
+                    WHERE id = $1;
+                `, [hashtag.hashtagId]);
+            }
+        }));
+
+
+        if (hashtags.length > 0) {
+
+            const uniqueHashtags = Array.from(new Set(hashtags));
+            const newHashtags = uniqueHashtags.filter(tag => !newPost.hashtags.some(existingTag => existingTag.hashtag === tag));
+
+
+            await Promise.all(newHashtags.map(async (tag) => {
+                const { rows: [existingTag] } = await db.query(`
+                    SELECT * FROM hashtags WHERE hashtag = $1;
+                `, [tag]);
+                console.log(existingTag)
+                if (existingTag !== undefined) {
+                    await db.query(`
+                    UPDATE hashtags SET total = total + 1 WHERE id = $1;
+                    `, [existingTag.id]);
+                } else {
+                    await db.query(`
+                    INSERT INTO hashtags (hashtag, total) VALUES ($1, 1);
+                    `, [tag]);
+                }
+
+                const { rows: [newTag] } = await db.query(`
+                    SELECT * FROM hashtags WHERE hashtag = $1;
+                `, [tag]);
+
+                await db.query(`
+                    INSERT INTO "postsHashtags" ("postId", "hashtagId") VALUES ($1, $2);
+                `, [newPost.id, newTag.id]);
+            }));
+        }
+
+        const { rows: [updatedPost] } = await db.query(`
+            SELECT p.*,
+                COALESCE(
+                    (SELECT json_agg(
+                        json_build_object(
+                            'hashtagId', h.id,
+                            'hashtag', h.hashtag
+                        )
+                    ) FROM "postsHashtags" AS ph
+                    LEFT JOIN hashtags AS h ON ph."hashtagId" = h.id
+                    WHERE ph."postId" = p.id), '[]') AS hashtags
+            FROM posts AS p
+            WHERE p.id = $1;
+        `, [id]);
+
+        res.status(200).send(updatedPost);
+    } catch (error) {
+        const errorMessage = error.message ? error.message : 'An internal server error occurred.';
+        res.status(500).send(errorMessage);
+    }
 }
