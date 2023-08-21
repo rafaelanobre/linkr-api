304126e069a6e808a3761fceaac5275ff00523c1
Willyan <wkinhaamaral@gmail.com>
2023-08-21 14:18:02 -0300

    fix: error message delet

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -139,9 +139,10 @@ export async function DeletePost(req, res) {
             `DELETE FROM posts WHERE "createdBy" = $1 AND "id" = $2`,
             [createdBy, postId]
         )
+        if(del.rowCount === 0) return res.status(401).send("you do not have authorization to delete this post")
         res.sendStatus(200);
     } catch (error) {
-        const errorMessage = error.message ? error.message : "Ocorreu um erro interno no servidor.";
+        const errorMessage = error.message ? error.message : "it was not possible to delete.";
         res.status(500).send(errorMessage);
     }
 
