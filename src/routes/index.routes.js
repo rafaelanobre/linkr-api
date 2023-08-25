2b87350a41e45f836899cbf2d243243cda8bb9fc
HenriqueSilva <HenriqueSilva254@gmail.com>
2023-08-24 21:00:11 -0300

    merge follow/comments

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,11 +4,8 @@ import authrouter from "./auth.routes.js";
 import postsRouter from "./posts.routes.js";
 import userRouter from "./user.routes.js";
 import hashtagsRouter from "./hashtags.routes.js";
-<<<<<<< HEAD
 import followRouter from "./follow.routes.js";
-=======
 import commentsRouter from "./comments.routes.js";
->>>>>>> 746f8b1ffdc673c709a8eaafa8e4e5fa9063d6f6
 
 const router = Router();
 
@@ -17,10 +14,7 @@ router.use(authrouter);
 router.use(postsRouter);
 router.use(userRouter);
 router.use(hashtagsRouter);
-<<<<<<< HEAD
 router.use(followRouter)
-=======
 router.use(commentsRouter);
->>>>>>> 746f8b1ffdc673c709a8eaafa8e4e5fa9063d6f6
 
 export default router;
