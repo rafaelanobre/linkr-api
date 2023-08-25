117e4fa49c317fc1645007140c763392071dea55
rafaelanobre <raafanobre@gmail.com>
2023-08-24 22:55:18 -0300

    :wrench: Fix: comments and get comments in posts

--- src/repositories/post.repository.js
+++ src/repositories/post.repository.js
@@ -68,6 +68,7 @@ export async function getTimelinePostsDB(){
         u.name AS "userName",
         u.photo AS "userPhoto",
         u.id AS "userId",
+        (SELECT COUNT(*) FROM comments c WHERE c."postId" = p.id) AS "commentCount",
         ARRAY_AGG(users.name) AS "usersLikedNames",
         COALESCE(
             json_agg(
--- src/routes/comments.routes.js
+++ src/routes/comments.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { validateAuth } from "../middlewares/validateAuth.js";
+import { insertNewComment, postComments } from "../controllers/comments.controller.js";
 
 const commentsRouter = Router();
 
-commentsRouter.get("/comments/:id", );
-commentsRouter.post("/comments/:id", validateAuth, );
+commentsRouter.get("/comments/:id", postComments);
+commentsRouter.post("/comments/:id", validateAuth, insertNewComment);
 
 export default commentsRouter;
